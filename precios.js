b16b00ac4d1f24736160fe645b7134af44143972
RS <112523550+robertoschwarz78@users.noreply.github.com>
2022-11-25 17:50:41 -0300

    commit 5

---
+++ precios.js
@@ -0,0 +1,19 @@
+
+// En este arhivo se pueden actualizar los precios o poner sin stock.
+
+console.log("actualizando precios");
+
+document.getElementById("precioHamb").innerText="$ 900"
+document.getElementById("precioBife").innerText="$ 1200"
+document.getElementById("precioPanc").innerText="$ 600"
+document.getElementById("precioPica").innerText="$ 1500"
+document.getElementById("precioTost").innerText="$ 900"
+document.getElementById("precioEnsa").innerText="$ 800"
+
+document.getElementById("precioCoca").innerText="$ 300"
+document.getElementById("precioAgua").innerText="$ 200"
+document.getElementById("precioCerv").innerText="$ 500"
+
+document.getElementById("precioFlan").innerText="$ 500"
+document.getElementById("precioFrut").innerText="no disponible"
+
